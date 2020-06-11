8e6f2444f3ced0ba46ae86d7acffbbd4028f92e0
Piero Trigoso <trigosop@gmail.com>
2020-06-10 22:22:51 -0300

    :metal: added welcome message

--- src/commands/index.js
+++ src/commands/index.js
@@ -8,14 +8,9 @@ const commands = {
 };
 
 module.exports = async (msg) => {
-  console.log(msg);
   if (msg.guild.id === guildID && msg.channel.id === channelID) {
     const args = msg.content.split(' ');
-    console.log(args);
-
-    if (args.length == 0 || args[0].charAt(0) !== '!') {
-      return;
-    }
+    if (args.length == 0 || args[0].charAt(0) !== '!') return;
     const command = args.shift().substr(1);
     if (Object.keys(commands).includes(command)) {
       commands[command](msg, args);
--- src/index.js
+++ src/index.js
@@ -11,4 +11,10 @@ client.once('ready', () => {
 
 client.on('message', commandHandler);
 
+client.on('guildMemberAdd', async (member) => {
+  await member.send(
+    '¡Bienvenid@ a Diablo Latino! Hacé click en el canal #⭐-elige-tu-clase para poder participar del server. Si jugás con varios personajes, no te preocupes, puedes cambiar la clase todas las veces que quieras. Nos vemos en Santuario!'
+  );
+});
+
 client.login(process.env.BOT_TOKEN);
